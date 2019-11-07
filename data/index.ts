8d9e9119870bd55bdddc244b2f3bd62785209683
Mukhammed Ismailov <mukhammed.ismailov@pluto.tv>
2019-11-07 18:31:28 +0300

    new lessons structure

---
+++ data/index.ts
@@ -0,0 +1,5 @@
+import * as lessons from './lessons.json';
+
+export function getData() {
+    return lessons;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import { botConfig, start } from './lib/telegram-bot';
-import { readFileSync } from 'fs';
+import { botConfig, start, sendMessage } from './lib/telegram-bot';
+import { getData } from '../data';
+import {IMessage} from "./lesson/DTO/IMessage";
 
+const data = getData();
 const bot = botConfig();
+const chat = { id: '419931906' };
+let count = 0;
 
-const chatIdFromFile: string = readFileSync('C:/git/bh24-bot/chatid.json', 'utf8');
-const chat = JSON.parse(chatIdFromFile);
-
-start(bot);
+const interval = setInterval(() => {
+    if (count < data.length) {
+        data[count].messages.forEach(message => {
+            sendMessage(bot, chat, message as IMessage);
+        });
+        count++;
+    } else {
+        clearInterval(interval);
+        count = 0;
+    }
+}, 10000);
